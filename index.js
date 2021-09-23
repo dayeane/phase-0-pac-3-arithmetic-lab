0c9638e604f6d251deb12ae565cc4d9179802a31
Dayeane <dayeane85@gmail.com>
2021-09-23 10:57:32 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,35 @@
+function add (a,b) {
+return a + b;
+ //=> 81
+}
+
+function subtract (a,b) {
+return a - b;
+ //=> 20
+}
+
+function multiply (a,b) {
+return a * b;
+//=> 6.8
+}
+
+function divide (a,b) {
+return a / b;
+//=> 2
+} 
+
+function increment (n) {
+    return ++n
+}
+
+function decrement (n) {
+    return --n
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string)
+}
